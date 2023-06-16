6263e2098952084f59689359bc323290df60be26
Aleksandar Grbic <hi@aleksandar.xyz>
2023-06-16 21:26:37 +0200

    export schemas

--- Schemas/index.js
+++ Schemas/index.js
@@ -5,11 +5,11 @@ import courseSectionSchemas from "./course-section.js";
 import courseSchemas from "./course.js";
 import ratingSchemas from "./rating.js";
 
-export const schemas = [
+export const schemas = {
     article: articleSchemas.article,
     comment: commentSchemas.comment,
     course: courseSchemas.course,
     courseLecture: courseLectureSchemas.courseLecture,
     courseSection: courseSectionSchemas.courseSection,
     rating: ratingSchemas.rating,
-];
+};
